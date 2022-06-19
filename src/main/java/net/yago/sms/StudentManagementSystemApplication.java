272baca1799094e4b666744de4de2e8349600742
Yago Aguiar <yagofelixaguiar@gmail.com>
2022-06-19 14:52:29 -0300

    Improving the page layout

--- src/main/java/net/yago/sms/StudentManagementSystemApplication.java
+++ src/main/java/net/yago/sms/StudentManagementSystemApplication.java
@@ -1,12 +1,11 @@
 package net.yago.sms;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import net.yago.sms.entity.Student;
-import net.yago.sms.repository.StudentRepository;
+
 
 @SpringBootApplication
 public class StudentManagementSystemApplication implements CommandLineRunner{
@@ -15,19 +14,11 @@ public class StudentManagementSystemApplication implements CommandLineRunner{
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
 	
-	@Autowired
-	private StudentRepository studentRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
-//		Student student1 = new Student("Yago","Aguiar","yaguin123@gmail.com");
-//		studentRepository.save(student1);
-//		
-//		Student student2 = new Student("Vie","Course","vmc@gmail.com");
-//		studentRepository.save(student2);
-//		
-//		Student student3 = new Student("Lua","Aguiar","luluguiar@gmail.com");
-//		studentRepository.save(student3);
+
 	}
 
 }
